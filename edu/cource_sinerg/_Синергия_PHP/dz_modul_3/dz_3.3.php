33f068acc888802aedddbb8978b0623f12c3ad37
[alt]legioner9 <legioner9@inbox.ru>
2023-08-25 11:48:01 +0700

    <> #3

---
+++ edu/cource_sinerg/_Синергия_PHP/dz_modul_3/dz_3.3.php
@@ -0,0 +1,12 @@
+<?php
+class Student {
+    public function sayHi(){
+        echo "Hi!";
+    }
+}
+
+$student = new Student();
+$student->sayHi();
+
+// Создайте класс Student с методом sayHi(), который должен выводить сообщение "Hi!" (без кавычек)
+?>
