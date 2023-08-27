93fd41f9beaa0bd79c046d730a002993b7fdf788
[alt]legioner9 <legioner9@inbox.ru>
2023-08-28 00:41:25 +0700

    <> dz_ #2

---
+++ _man/keywords/return/v1.php
@@ -0,0 +1,17 @@
+<?php
+
+function f0()
+{
+    return false;
+}
+function f1()
+{
+    return true;
+}
+
+if (f0()){
+    echo "f0";
+}
+if (f1()){
+    echo "f1";
+}
--- edu/cource_sinerg/_Синергия_PHP/dz_modul_4/dz_3.php
+++ edu/cource_sinerg/_Синергия_PHP/dz_modul_4/dz_3.php
@@ -3,30 +3,39 @@
 // Напишите программу, которая будет получать на входе две строки и выводить на экран, является ли вторая строка анаграммой
 // первой (т.е. состоит ли она из тех же букв в обратном порядке)
 
-$str1 = $argv[1];
-$str2 = $argv[2];
+function is_anagram($str1, $str2)
+{
 
-$strlen1 = strlen($str1);
-$strlen2 = strlen($str2);
-
-$arr1 = str_split($str1);
-$arr2 = str_split($str2);
-
-var_dump($arr1);
-var_dump($arr2);
-
-if ($strlen1 != $strlen2) {
-    echo "Строки не являются анаграммами друг друга\n";
-} else {
     $arr1 = str_split($str1);
     $arr2 = str_split($str2);
 
-    var_dump($arr1); 
-    var_dump($arr2);
-
-    if ($arr1 === $arr2) {
-        echo "Строки являются анаграммами друг друга\n";
+    $len1 = count($arr1);
+    $len2 = count($arr2);
+    
+    if ($len1 != $len2) {
+        return false;
     } else {
-        echo "Строки не являются анаграммами друг друга\n";
+
+        for ($i = 0; $i < count($arr1); $i++) {
+
+            echo $arr1[$i], "\n";
+            echo $arr2[$len2 - $i - 1], "\n";
+
+            if ($arr1[$i] != $arr2[$len2 - $i - 1]) {
+                return false;
+            }
+        }
+        return true;
     }
+}
+
+if ($argc != 3) {
+    echo "$argc != 3 \n";
+    return 1;
+}
+
+if (is_anagram($str1, $str2)) {
+    echo "strings is anagram\n";
+} else {
+    echo "strings is not anagram\n";
 }
