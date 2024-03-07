076504e011b04981a9e8d74b5a352b3e0729ffd9
[alt]legioner9 <legioner9@inbox.ru>
2024-03-07 11:18:46 +0700

    <> ##

---
+++ .d/.arb/php_doc.arb/function.var-dump.php.html.ram/.grot/man.php
@@ -0,0 +1,6 @@
+<? 
+
+$arr = array(1,2,array("a", "b" , "c"));
+
+var_dump($arr)
+
---
+++ .d/.arb/php_doc.arb/language.types.intro.php.html.ram/.grot/man.php
@@ -0,0 +1,11 @@
+<? 
+
+$boolean = true;
+$string_1="vdsvdv";
+$string_2='asdcsdc';
+$int = 12;
+ 
+echo get_debug_type($boolean),"\n";
+echo get_debug_type($string_1),"\n";
+echo get_debug_type($string_2),"\n";
+echo get_debug_type($int),"\n";
