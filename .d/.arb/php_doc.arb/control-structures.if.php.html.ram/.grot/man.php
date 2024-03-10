704d2e6aa412ef7675e98a09f2a8e70afe6bb747
[alt]legioner9 <legioner9@inbox.ru>
2024-03-10 20:30:54 +0700

    <> ##

---
+++ .d/.arb/php_doc.arb/control-structures.if.php.html.ram/.grot/man.php
@@ -0,0 +1,17 @@
+<?
+
+$a = 5;
+$b = 4;
+
+if ($a > $b)
+    echo "a больше! b\n"; ?>
+
+<?php if (true): ?>
+
+    html code to run if condition is true
+
+<?php else: ?>
+
+    html code to run if condition is false
+
+<?php endif ?>
